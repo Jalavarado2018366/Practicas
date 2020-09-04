69adb4897d76fc9d8085fcece8ffe8b55f452121
Thomas Pamal <tpamalkinal@gmail.com>
2020-09-04 12:09:51 -0600

    Splash Screen

--- app/src/main/java/com/example/practicas/MainActivity.java
+++ app/src/main/java/com/example/practicas/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.practicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,Pantalla_Principal.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        },3000);
     }
 }
---
+++ app/src/main/java/com/example/practicas/Pantalla_Principal.java
@@ -0,0 +1,14 @@
+package com.example.practicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla__principal);
+    }
+}
