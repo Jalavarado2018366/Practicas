125c1784fd8fdfca9e38395d349f1666eb83c6fd
Jalavarado2018366 <54085068+Jalavarado2018366@users.noreply.github.com>
2020-09-07 14:41:33 -0600

    Diseño en login

--- app/src/main/java/com/example/practicas/Pantalla_Principal.java
+++ app/src/main/java/com/example/practicas/Pantalla_Principal.java
@@ -2,13 +2,44 @@ package com.example.practicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Pantalla_Principal extends AppCompatActivity {
-
+    private EditText et1,et2;
+    private Button buttonOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla__principal);
+
+        et1 = findViewById(R.id.editTextTextPersonName);
+        et2 = findViewById(R.id.editTextTextPassword);
+
+        buttonOk= findViewById(R.id.button);
+
+        if (et1.length() > 0 ){
+            buttonOk.setEnabled(true);
+        }else{
+            buttonOk.setEnabled(false);
+        }
+        if (et2.length() > 0 ){
+            buttonOk.setEnabled(true);
+        }else{
+            buttonOk.setEnabled(false);
+        }
+    }
+    private void Calcular(String view){
+
+    }
+    private void AbrirWhatsApp(String telefono){
+        Intent _intencion = new Intent("android.intent.action.MAIN");
+        _intencion.setComponent(new ComponentName("com.whatsapp","com.whatsapp.Conversation"));
+        _intencion.putExtra("jid", PhoneNumberUtils.stripSeparators("502" + telefono)+"@s.whatsapp.net");
+        startActivity(_intencion);
     }
 }
