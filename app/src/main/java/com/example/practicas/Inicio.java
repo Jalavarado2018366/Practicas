5cba15b8e7ca80c51af98a3502a74b0dea2f9735
Thomas Pamal <tpamalkinal@gmail.com>
2020-09-09 13:32:18 -0600

    Login -

--- app/src/main/java/com/example/practicas/Inicio.java
+++ app/src/main/java/com/example/practicas/Inicio.java
@@ -1,14 +1,19 @@
 package com.example.practicas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Inicio extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+
     }
 }
